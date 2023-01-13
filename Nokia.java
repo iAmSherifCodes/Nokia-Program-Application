b65c1adfd53dfe536613d79b54860eca5bbde3e0
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-01-13 05:09:43 +0100

    Added more options to phonebook Menu

--- Nokia.java
+++ Nokia.java
@@ -2,6 +2,7 @@ package Nokia;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.spi.AbstractResourceBundleProvider;
 
 public class Nokia {
     Scanner scan = new Scanner(System.in);
@@ -73,45 +74,101 @@ public class Nokia {
         switch (userPhonebookChoice){
             case "1":
                 System.out.println("""
-                Type a name to search: 
+                Type a name to search:
                 or 'back' to return to previous menu
                 """);
                 String nameToSearch = scan.next();
-                if (nameToSearch.equalsIgnoreCase("back")){
-                    phonebook();
-                    break;
-                }else {
-                    System.out.println(phonebook.contains(nameToSearch) ? "The" + nameToSearch + "is in contact" : "The" + nameToSearch + "is not in contact");
+                phonebookBackMethod(nameToSearch);
+                if (!nameToSearch.equalsIgnoreCase("back")) {
+
+                    if (phonebook.isEmpty()){
+                        System.out.println("No contact in phonebook");
+                    }
+                    else {
+                        System.out.println(phonebook.contains(nameToSearch) ? nameToSearch + " is in contact" :  nameToSearch + " is not in contact");
+                    }
                     System.out.println("Type back to return to previous menu");
                     nameToSearch = scan.next();
-                    if (nameToSearch.equalsIgnoreCase("back")){
-                        phonebook();
-                        break;
-                    }
+                    phonebookBackMethod(nameToSearch);
+                    break;
                 }
                 break;
+
             case "2":
                 System.out.println("""
                 Type a service Number:
                 or 'back' to return to previous menu
                 """);
                 String serviceNo = scan.next();
-                if (serviceNo.equalsIgnoreCase("back")){
-                    phonebook();
-                    break;
-                }else {
+                phonebookBackMethod(serviceNo);
+                if (!serviceNo.equalsIgnoreCase("back")) {
                     System.out.println("Service No entered.");
                     System.out.println();
                     System.out.println("Type back to return to previous menu");
                     serviceNo = scan.next();
-                    if (serviceNo.equalsIgnoreCase("back")){
-                        phonebook();
-                        break;
-                    }
+                    phonebookBackMethod(serviceNo);
+                    break;
                 }
                 break;
+
+            case "3":
+                System.out.println("Add Name");
+                String nameToAdd = scan.next();
+                phonebook.add(nameToAdd);
+                System.out.println(nameToAdd+" has been added successfully!");
+                System.out.println("Type 'back' to return to previous menu");
+                nameToAdd = scan.next();
+                phonebookBackMethod(nameToAdd);
+                break;
+
+            case "4":
+                System.out.println("""
+                        Type erase to erase all contact Or
+                        Type back to return to previous menu
+                        """);
+                String eraseOption = scan.next();
+                switch (eraseOption){
+                    case "erase": phonebook.clear(); break;
+                    case "back" : phonebookBackMethod(eraseOption);break;
+                    default:
+                        errorMessage("!!ERROR 41Ten!! Invalid input! Try Again");
+                        phonebook();
+                        break;
+                }
+
+            case "5":
+                if (phonebook.isEmpty()){
+                    System.out.println("No contact in phonebook\nType back to return to previous menu");
+                    String option = scan.next();
+                    phonebookBackMethod(option);
+                }else {
+                    //List the contacts for the user
+                    for (String j : phonebook){
+                        System.out.println(j);
+                    }
+                    //Prompt user to edit contact if they wish to
+                    System.out.println("Which name do you wish to edit? ");
+                    String contactToEdit = scan.next();
+                    if (phonebook.contains(contactToEdit)){
+                        phonebook.remove(contactToEdit);
+                        System.out.println("Enter new name");
+                        contactToEdit = scan.nextLine();
+                        phonebook.add(contactToEdit);
+                        System.out.println("Contact Edited Successfully");
+                        System.out.println();
+                        phonebook();
+                    }
+                }
 
         }
 
     }
+
+    public void phonebookBackMethod(String variableParameter){
+        if (variableParameter.equalsIgnoreCase("back")){
+            phonebook();
+        }
+
+    }
+
 }
