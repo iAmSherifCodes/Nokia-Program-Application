79858c67abe79db6b3d7e2ad58c7f6af77357129
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-01-16 02:48:17 +0100

    Fixed bug and Worked on message menu

--- Nokia.java
+++ Nokia.java
@@ -5,17 +5,10 @@ import java.util.Scanner;
 
 public class Nokia {
     private ArrayList<String> contacts = new ArrayList<>();
-    Scanner scan = new Scanner(System.in);
-    //To return to previous menu
-    //when the user type (back) we call the previous menu in the condition statement
-    // if (userInput.equalsIgnoreCase("back")) previousMenu();
 
-//    public void addToArray(String element){
-//        contacts.add(element);
-//    }
-    public void addToArray(int index, String element){
-        contacts.add(index, element);
-    }
+    private ArrayList<String> messagesDraft = new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
+
     public void errorMessage(String message){
         System.out.println(message);
     }
@@ -104,7 +97,6 @@ public class Nokia {
             case "2":
                 previousMenuPrompt("service Number:");
                 String serviceNo = scan.nextLine();
-                serviceNo = scan.nextLine();
                 phonebookBackMethod(serviceNo);
                 if (!serviceNo.equalsIgnoreCase("back")) {
                     System.out.println("Service No entered.");
@@ -120,7 +112,7 @@ public class Nokia {
                 System.out.println("Add Name");
                 String nameToAdd = scan.nextLine();
                 //addToArray(nameToAdd);
-                addToArray(nameToAdd);
+                addToContacts(nameToAdd);
                 System.out.println(nameToAdd+" has been added successfully!");
                 System.out.println(this.contacts);
                 previousMenuPrompt();
@@ -259,9 +251,12 @@ public class Nokia {
 
     }
 
-    private void addToArray(String nameToAdd) {
+
+
+    private void addToContacts(String nameToAdd) {
         this.contacts.add(nameToAdd);
     }
+
 
     public void previousMenuPrompt(String prompt){
         System.out.printf("Type a %s or back to return to previous menu",prompt);
@@ -278,8 +273,203 @@ public class Nokia {
 
     }
 
-    public void message(){
+    public void messageBackMethod(String variableParameter){
+        if (variableParameter.equalsIgnoreCase("back")){
+            message();
+        }
 
+    }
+
+
+    public void message(){
+        System.out.println("""
+                Enter:
+                1  --> Write messages
+                2  --> Inbox
+                3  --> Outbox
+                4  --> Draft
+                5  --> Picture messages
+                6  --> Templates
+                7  --> Smileys
+                8  --> Messages settings
+                9  --> Info Service
+                10  --> Voice mailbox number
+                11 --> Service command editor
+                back --> Return to previous menu
+                """);
+
+
+        String userMessageChoice = scan.next();
+        scan.nextLine();
+
+        switch (userMessageChoice){
+            case "1":
+                previousMenuPrompt("Message");
+                String userMessage = scan.nextLine();
+//                nameToSearch = scan.nextLine();
+                messageBackMethod(userMessage);
+                if (!userMessage.equalsIgnoreCase("back")) {
+                    this.messagesDraft.add(userMessage);
+
+                    System.out.println();
+                    System.out.println("Your message has been added to draft");
+                    System.out.println();
+
+//                    if (this.messagesDraft.isEmpty()){
+//                        System.out.println("No contact in phonebook");
+//                        System.out.println();
+//                    }
+//                    else {
+//                        System.out.println(this.messagesDraft.contains(userMessage) ? userMessage + " is in contact" :  userMessage + " is not in contact");
+//                    }
+                    previousMenuPrompt();
+                    userMessage = scan.next();
+                    messageBackMethod(userMessage);
+                }
+                break;
+
+            case "2":
+                System.out.println("Inbox is empty! ");
+                previousMenuPrompt();
+                String userInboxChoice = scan.next();
+                messageBackMethod(userInboxChoice);
+//                if (!serviceNo.equalsIgnoreCase("back")) {
+//                    System.out.println("Service No entered.");
+//                    System.out.println();
+//                    previousMenuPrompt();
+//                    serviceNo = scan.next();
+//                    phonebookBackMethod(serviceNo);
+//                    break;
+//                }
+                break;
+
+            case "3":
+                System.out.println("Outbox is empty! ");
+                previousMenuPrompt();
+                String userOutboxChoice = scan.next();
+                messageBackMethod(userOutboxChoice);
+                break;
+
+            case "4":
+                previousMenuPrompt("view to view all messages in draft");
+                String userDraftChoice = scan.nextLine();
+                switch (userDraftChoice){
+                    case "view":
+                        if (this.messagesDraft.isEmpty()){
+                            System.out.println("No message stored yet! ");
+                            System.out.println();
+                            previousMenuPrompt();
+                            String c = scan.nextLine();
+                            messageBackMethod(c);
+                        }else {
+                            //List all messages for the user
+                            for (int message = 1 ; message <= messagesDraft.size(); message++) {
+                                System.out.println(message + " - " + messagesDraft.get(message));
+                            }
+                        }
+                        break;
+
+                    case "back":
+                        message();
+                        break;
+
+                    default:
+                        errorMessage("!!ERROR 41Ten!! Invalid input! Try Again");
+                        message();
+                        break;
+                }
+                break;
+
+            case "5":
+                System.out.println("No templates available");
+                previousMenuPrompt();
+                String template = scan.next();
+                messageBackMethod(template);
+                break;
+
+            case "6":
+                System.out.println("Smileys");
+                String smileys[] = new String[5];
+                smileys[0] = "❤️😁😍💕🤷‍🫡"; smileys[1] = "😊"; smileys[2] = "👌"; smileys[3] ="😒"; smileys[4]= "😎";
+                for(String smiley : smileys){
+                    System.out.println(smiley);
+                }
+                previousMenuPrompt();
+                String smileyChoice = scan.next();
+                smileyChoice =scan.nextLine();
+                messageBackMethod(smileyChoice);
+                break;
+
+            case "7":
+                previousMenuPrompt("receipient name");
+                String receipient = scan.nextLine();
+                receipient = scan.nextLine();
+                phonebookBackMethod(receipient);
+                if(!receipient.equalsIgnoreCase("back")){
+                    System.out.println("Birthday Card Sent to "+ receipient);
+                    previousMenuPrompt();
+                    receipient = scan.next();
+                    phonebookBackMethod(receipient);
+                }
+
+                break;
+
+            case "8":
+                System.out.println("""
+                        1 --> Type of view
+                        2 --> Memory Status
+                        """);
+                choice = scan.nextLine();
+                choice = scan.nextLine();
+                switch (choice) {
+                    case "1" -> {
+                        System.out.println("Type of view");
+                        previousMenuPrompt();
+                        choice = scan.next();
+                        phonebookBackMethod(choice);
+                    }
+                    case "2" -> {
+                        System.out.println("Memory status");
+                        previousMenuPrompt();
+                        choice = scan.next();
+                        phonebookBackMethod(choice);
+                    }
+                    case "back" -> {
+                        phonebookBackMethod(choice);
+                        previousMenuPrompt();
+                        choice = scan.next();
+                        phonebookBackMethod(choice);
+                    }
+                }
+                break;
+
+            case "9":
+                System.out.println("Enter Speed Dial number");
+                int speedDial = scan.nextInt();
+                System.out.printf("%d has been added to speed dial ", speedDial);
+                previousMenuPrompt();
+                choice = scan.next();
+                phonebookBackMethod(choice);
+                break;
+
+            case "10":
+                System.out.println("You've added a voice tag!");
+                previousMenuPrompt();
+                choice = scan.next();
+                phonebookBackMethod(choice);
+                break;
+            case "back":
+                menu();
+                break;
+
+            default:
+                errorMessage("Invalid Input! Try Again"); phonebook();
+                break;
+        }
+    }
+
+    private void addToMessages(String nameToAdd) {
+        this.messagesDraft.add(nameToAdd);
     }
 
 }
